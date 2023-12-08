876534c26d6f9b4e95508b2bf4ee906e2bfe7f69
yhunlu <yhunlu87@gmail.com>
2023-12-08 23:13:52 +0300

    activate react.query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
