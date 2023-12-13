0248f4022162e0c727d4207cbecc56ed31d01496
Yahya Unlu <yhunlu87@gmail.com>
2023-12-13 20:16:57 +0300

    mutation progress

--- src/components/ToDoForm.tsx
+++ src/components/ToDoForm.tsx
@@ -19,34 +19,45 @@ const ToDoForm = () => {
         const updatedTodos = [savedTodo, ...(todos || [])];
         return updatedTodos as Todo[];
       });
+
+      ref.current!.value = '';
     },
   });
 
   const ref = useRef<HTMLInputElement>(null);
 
   return (
-    <form
-      className="row mb-3"
-      onSubmit={(event) => {
-        event.preventDefault();
+    <>
+      {addTodo.error && (
+        <div className="alert alert-danger">{addTodo.error.message}</div>
+      )}
+      <form
+        className="row mb-3"
+        onSubmit={(event) => {
+          event.preventDefault();
 
-        addTodo.mutate({
-          id: 0,
-          title: ref.current!.value,
-          completed: false,
-          userId: 1,
-        });
-      }}
-    >
-      <div className="col">
-        <input type="text" ref={ref} className="form-control" />
-      </div>
-      <div className="col">
-        <button className="btn btn-primary" type="submit">
-          Add
-        </button>
-      </div>
-    </form>
+          addTodo.mutate({
+            id: 0,
+            title: ref.current!.value,
+            completed: false,
+            userId: 1,
+          });
+        }}
+      >
+        <div className="col">
+          <input type="text" ref={ref} className="form-control" />
+        </div>
+        <div className="col">
+          <button
+            className="btn btn-primary"
+            type="submit"
+            disabled={addTodo.isPending}
+          >
+            {addTodo.isPending ? 'Adding...' : 'Add'}
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
