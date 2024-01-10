c73f1042701abdd736eaeb0fc337451e35dc3b6b
yhunlu <yhunlu87@gmail.com>
2024-01-11 02:52:11 +0300

    Private Routes

--- src/routing/UsersPage.tsx
+++ src/routing/UsersPage.tsx
@@ -1,15 +1,20 @@
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
 import UserLists from './UserLists'
+import useAuth from './hooks/useAuth'
 
 const UsersPage = () => {
+  const { user } = useAuth();
+  if (!user)
+    return <Navigate to="/login" />
+
   return (
     <div className="row">
-        <div className="col">
-            <UserLists />
-        </div>
-        <div className="col">
-            <Outlet />
-        </div>
+      <div className="col">
+        <UserLists />
+      </div>
+      <div className="col">
+        <Outlet />
+      </div>
     </div>
   )
 }
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -5,6 +5,7 @@ import Layout from './Layout';
 import UserDetail from './UserDetail';
 import UsersPage from './UsersPage';
 import ErrorPage from './ErrorPage';
+import LoginPage from './LoginPage';
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <HomePage />,
+      },
+      {
+        path: 'login',
+        element: <LoginPage />,
       },
       {
         path: 'users',
